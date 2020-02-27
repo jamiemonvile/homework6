8ca91583984c5b5c86f012e3c6c21cf4ee719f92
jamiemonvile <59850787+jamiemonvile@users.noreply.github.com>
2020-02-27 14:32:54 -0500

    Add files via upload

---
+++ js/js/video.js
@@ -0,0 +1,69 @@
+var video;
+
+function getVid(){
+	video = document.querySelector("#myVideo"); 
+}
+
+function playVid() { 
+	video.play();
+	let vol = document.querySelector("#volume");
+	vol.innerHTML=video.volume *100 +"%";
+	console.log("Play Video");
+	console.log(video);
+} 
+
+function pauseVid() { 
+	video.pause();
+	console.log("Pause Video");
+} 
+
+function decreaseSpeed() { 
+	video.playbackRate /= 1.25;
+  	console.log("Speed is "+ video.playbackRate);
+} 
+
+function increaseSpeed() {
+	video.playbackRate *= 1.25;
+	console.log("Speed is "+ video.playbackRate);
+} 
+
+function skipAhead() {
+	video.currentTime = video.currentTime + 60;
+	video.play();
+	console.log("Current location is "+ video.currentTime);
+	if (video.currentTime == 0) {
+		video.playbackRate = 1;
+	}
+} 
+
+function mute() { 
+  	if (video.muted){
+  		console.log("Changing to Unmuted");
+  		video.muted = false;
+  		document.querySelector("#mute") .innerHTML = "Mute"
+  	}
+  	else{
+      	console.log("Changing to Muted");
+      	video.muted = true;
+      	document.querySelector("#mute") .innerHTML = "Unmute"
+  	}
+}
+
+function changeVolume(val) {
+	let slider = document.querySelector('#volumeSlider');
+	video.volume = slider.value / 100;
+	console.log("Volume is " + video.volume *100);
+	let newvol = document.querySelector("#volume")
+	newvol.innerHTML=video.volume *100 + "%";
+}
+       
+
+function gray() { 
+	document.querySelector("#myVideo").classList.add("grayscale")
+	console.log("In grayscale")
+}
+
+function color() {
+	document.querySelector("#myVideo").classList.remove("grayscale")
+	console.log("In color") 
+}
